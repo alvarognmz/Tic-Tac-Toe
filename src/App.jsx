7e24d141ec83ca124d05bce01e1933ab6df91cfb
alvarognmz <alvarognmz@gmail.com>
2025-10-20 11:20:08 +0200

    First commit. Tic-Tac-Toe

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import { useState } from 'react'
+import './App.css'
+import confetti from 'canvas-confetti'
+import { Square } from '../components/Square'
+import { TURNS } from './constants'
+import { checkWinnerFrom } from '../logic/board'
+import { WinnerModal } from '../components/WinnerModal'
+import { checkEndGame } from '../logic/board'
+
+
+
+function App() {
+  
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  }) // Estado definido. Nos referimos al tablero
+  const [turn,setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
+  const [winner, setWinner] = useState(null) // null no hay ganador, false empate
+
+  const updateBoard = (index) => {
+    // Si hay algo en esa casilla/índice, no hace nada
+    if(board[index] || winner) return
+    // Actualiza el tablero
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+    // Cambio de turnos
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+    //GUardar partida
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', newTurn)
+    // revisar si hay un ganador
+    const newWinner = checkWinnerFrom(newBoard)
+    if (newWinner) {
+      confetti()
+      setWinner(newWinner)
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false)
+    }
+  }
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
+  }
+
+  return(
+    <main className = 'board'>
+      <h1>TicTacToe</h1>
+      <button onClick={resetGame}>Reset Game</button>
+      <section className = 'game'>
+        {
+          board.map((_, index) => {
+            return (
+              <Square
+                key = {index}
+                index = {index}
+                updateBoard = {updateBoard}
+              > {board[index]}</Square>
+            )
+          })
+        }
+      </section>
+
+      <section className = 'turn'>
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+
+      <WinnerModal resetGame = {resetGame} winner = {winner}></WinnerModal>
+    </main>
+    
+  )
+}
+
+export default App
